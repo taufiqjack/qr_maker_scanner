9840749c645082c63701459da8b4db18836527fa
taufiqjack <taufiqdwic@gmail.com>
2022-12-28 09:56:20 +0700

    change font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:qr_maker_scan/utils/route.dart';
 import 'package:qr_maker_scan/views/splash_view.dart';
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       navigatorKey: Go.navigatorKey,
       theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
         primarySwatch: Colors.blue,
       ),
       home: const SplashView(),
