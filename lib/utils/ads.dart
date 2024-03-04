3abd6ab529a3ce2d096495d1c6bdef9caca58ea9
taufiqjack <taufiqdwic@gmail.com>
2024-03-04 14:04:14 +0700

    update: update for ads another account

--- lib/utils/ads.dart
+++ lib/utils/ads.dart
@@ -1,4 +1,4 @@
-String bannerAds = 'ca-app-pub-5262991443160331/3567340802';
+String bannerAds = 'ca-app-pub-9664445573032616/9436773171';
 String sample = 'ca-app-pub-3940256099942544/6300978111';
 String sampleInter = 'ca-app-pub-3940256099942544/1033173712';
 String intersAds = 'ca-app-pub-5262991443160331/6237156583';
